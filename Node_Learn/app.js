9f4c8dce97235e0a9758c89a637b5f705816d66d
zhangshuai <13522325228@163.com>
2017-10-17 17:52:34 +0800

    新增input.txt/main.js测试node.js的异步读取文件

--- Node_Learn/app.js
+++ Node_Learn/app.js
@@ -1,11 +1,11 @@
 var http = require('http');
 
 var hostname = '127.0.0.1';
-var port = 3000;
-console.log("HTTP server is listening at port 3000.");
+var port = 8081;
+console.log("HTTP server is listening at port 8081.");
 var server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-type':'text/html'});
-    res.end('<h1>Hello NodeJS</h1>');
+    res.end('<h1>Hello Nodejs</h1>');
 });
 
 server.listen(port, hostname, () => {
